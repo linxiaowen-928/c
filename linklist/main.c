c787aa08a3492db7192da4ad10667bbdee61e4be
linxiaowen <2874582127@qq.com>
2021-06-11 21:43:05 +0800

    链表

---
+++ linklist/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+int main(int argc, char *argv[]) 
+{
+	int number;
+	List list;
+	list.head = NULL;
+	do{
+		scanf("%d",&number); 
+		if(number!=-1){
+			add(&list,number);
+		}
+		
+	}while(number!=-1);
+	Node* p= list.head;
+	print(p);
+	return 0;
+}
+void add(List *pList,int number)
+{
+	Node *p=(Node*)malloc(sizeof(Node));
+	p->value = number;
+	p->next = NULL;
+	Node* last = pList->head;
+	if(last){
+		while(last->next){
+			last=last->next;
+		}
+		last->next=p;
+	}else{
+		pList->head = p;
+	}
+}
+void print(Node *p)
+{
+	int count = 1;
+	printf("%d\n",p->value);
+	do{
+		
+		printf("p%d->value=%d\n",count,p->value);
+		count++;
+		p=p->next;
+	}while(p->next);
+	printf("p%d->value=%d\n",count,p->value);
+}
---
+++ linklist/struct.h
@@ -0,0 +1,12 @@
+#ifndef _NODE_H_
+#define _NODE_H_
+typedef struct _node{
+	int value;
+	struct _node* next;
+}Node;
+typedef struct _list{
+	Node *head;
+}List;
+void add(List *plist,int number);
+void print(Node *p);
+#endif
